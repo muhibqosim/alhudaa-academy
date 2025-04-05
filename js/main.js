ef2325ce5141bd4aee601c4dd542692195363f4b
muhibqosim@gmail.com <muhibqosim@gmail.com>
2025-04-05 08:56:03 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function myFunction(x) {
+            x.classList.toggle("change");
+        }
+        function toggleMenu() {
+            var menu = document.getElementById("mobile-menu");
+            if (menu.classList.contains("hidden")) {
+                menu.classList.remove("hidden");
+            } else {
+                menu.classList.add("hidden");
+            }
+        } 
+        
+        
+        
+  //JS TYPEWR
+  
+  
+   /*WhatsApp me anchor */
+  document.getElementById("messageForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var message = document.getElementById("messageInput").value;
+    var encodedMessage = encodeURIComponent(message);
+    var whatsappLink = "https://wa.me/2348029675288/?text=" + encodedMessage;
+      // Replace 'whatsapp-number' with your WhatsApp phone number, including the country code.
+    // For example, if your WhatsApp number is +1234567890, the link would be: https://wa.me/1234567890/?text=...
+
+    window.location.href = whatsappLink;
+  });
+
+  
+  
+  AOS.init({
+    offset: 200,
+    duration: 600,
+    easing: 'ease-in-sine',
+    delay: 200,
+    once: true
+  }, );
