bf56dd28b490200c77b4f64dca1ba26169e9a7b8
muhibqosim@gmail.com <muhibqosim@gmail.com>
2025-04-05 11:56:15 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,56 +1,57 @@
 function myFunction(x) {
-            x.classList.toggle("change");
-        }
-        function toggleMenu() {
-            var menu = document.getElementById("mobile-menu");
-            if (menu.classList.contains("hidden")) {
-                menu.classList.remove("hidden");
-            } else {
-                menu.classList.add("hidden");
-            }
-        } 
-        
-        
-        
-  //JS TYPEWR
-  
-  
-  
-  const swiper = new Swiper(".mySwiper", {
-    loop: true,
-    effect: "fade",
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    speed: 1000,
-  });
+  x.classList.toggle("change");
+}
+
+function toggleMenu() {
+  var menu = document.getElementById("mobile-menu");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+}
 
 
 
-  
-   /*WhatsApp me anchor */
-  document.getElementById("messageForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var message = document.getElementById("messageInput").value;
-    var encodedMessage = encodeURIComponent(message);
-    var whatsappLink = "https://wa.me/2348029675288/?text=" + encodedMessage;
-      // Replace 'whatsapp-number' with your WhatsApp phone number, including the country code.
-    // For example, if your WhatsApp number is +1234567890, the link would be: https://wa.me/1234567890/?text=...
+//JS TYPEWR
 
-    window.location.href = whatsappLink;
-  });
 
-  
-  
-  AOS.init({
-    offset: 200,
-    duration: 600,
-    easing: 'ease-in-sine',
-    delay: 200,
-    once: true
-  }, );
+
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  effect: "fade",
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  speed: 1000,
+});
+
+
+
+
+/*WhatsApp me anchor */
+document.getElementById("messageForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var message = document.getElementById("messageInput").value;
+  var encodedMessage = encodeURIComponent(message);
+  var whatsappLink = "https://wa.me/2348029675288/?text=" + encodedMessage;
+  // Replace 'whatsapp-number' with your WhatsApp phone number, including the country code.
+  // For example, if your WhatsApp number is +1234567890, the link would be: https://wa.me/1234567890/?text=...
+
+  window.location.href = whatsappLink;
+});
+
+
+
+AOS.init({
+  offset: 200,
+  duration: 600,
+  easing: 'ease-in-sine',
+  delay: 200,
+  once: true
+}, );
